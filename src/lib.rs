05ce98d24c71b48fa70898420636de58c7a57ce4
Max Zargov <max@zargovv.com>
2025-10-18 03:08:32 +0500

    move `GCHandshake` trait to `proto-common`

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,7 @@ pub use steam_vent_proto as proto;
 
 pub use connection::{Connection, ConnectionTrait, ReadonlyConnection};
 pub use eresult::EResult;
-pub use game_coordinator::{
-    handshake::{GCHandshake, GenericGCHandshake},
-    GameCoordinator,
-};
+pub use game_coordinator::{GameCoordinator, handshake::GenericGCHandshake};
 pub use message::NetMessage;
 pub use net::{NetworkError, RawNetMessage};
 pub use serverlist::{DiscoverOptions, ServerDiscoveryError, ServerList};
